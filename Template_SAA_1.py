64e89e52b6412b58bc603a180b539b6fd5c1f935
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-10-21 17:58:58 +0530

    Create Template_SAA_1.py

---
+++ Template_SAA_1.py
@@ -0,0 +1,23 @@
+import turtle
+
+#Write the lines for taking user input here
+
+#Write the for loop for square creation
+
+turtle.penup()
+turtle.goto(100, 100)
+turtle.pendown()
+
+#Write the for loop for square creation
+
+turtle.penup()
+turtle.goto(0, 100)
+turtle.pendown()
+
+#Write the for loop for square creation
+
+turtle.penup()
+turtle.goto(100, 0)
+turtle.pendown()
+
+#Write the for loop for square creation
